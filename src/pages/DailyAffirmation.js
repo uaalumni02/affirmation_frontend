fb6918a9b824a2db4235f55f817c62301fc0fbf0
uaalumni02 <dbell1911@gmail.com>
2025-01-12 18:57:21 -0600

    start random affirmation pg

--- src/pages/DailyAffirmation.js
+++ src/pages/DailyAffirmation.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import "../static/dailyAffirmation.css";
 
 const DailyAffirmation = () => {
   const [name, setName] = useState("");
+  const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const fetchUserData = () => {
     const url = window.location.pathname;
@@ -17,17 +20,39 @@ const DailyAffirmation = () => {
       .catch((error) => console.error("Error:", error));
   };
 
+  const fetchCategoryData = () => {
+    fetch("http://localhost:3000/api/category/", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        setCategory(response.data);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   useEffect(() => {
     fetchUserData();
+    fetchCategoryData();
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <h1>Welcome {name}!</h1>
-
-      {/* <button onClick={generateRandomAffirmation}>
-        Generate New Affirmation
-      </button> */}
+      <p className="affirmation"></p>
+      <button className="btn">Generate New Affirmation</button>
+      <select
+        className="dropdown"
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        <option value="">Select Category</option>
+        {category.map((cat, index) => (
+          <option key={index} value={cat.category}>{cat.category}</option>
+        ))}
+      </select>
+      <button className="btn" onClick={() => window.location.href='/create-affirmation'}>Create Affirmation</button>
     </div>
   );
 };
