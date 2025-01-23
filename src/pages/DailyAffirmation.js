dadf82ef68bdfd021509c10cf6567f867239a778
uaalumni02 <dbell1911@gmail.com>
2025-01-22 21:14:10 -0600

    start random affirmation function

--- src/pages/DailyAffirmation.js
+++ src/pages/DailyAffirmation.js
@@ -5,6 +5,7 @@ const DailyAffirmation = () => {
   const [name, setName] = useState("");
   const [category, setCategory] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [categoryId, setCategoryId] = useState("");
 
   const fetchUserData = () => {
     const url = window.location.pathname;
@@ -37,22 +38,68 @@ const DailyAffirmation = () => {
     fetchCategoryData();
   }, []);
 
+
+
+
+
+
+//-----this function needs to pass the Id of the category selected
+
+
+  const fetchRandomAffirmation = () => {
+    fetch("http://localhost:3000/api/random_affirmation/", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+
+
+
+
+
+
+
+
+
+  const handleCategoryChange = (e) => {
+    const selectedCat = category.find((cat) => cat.category === e.target.value);
+    setSelectedCategory(selectedCat.category);
+    setCategoryId(selectedCat._id);
+    console.log(categoryId);
+  };
+
   return (
     <div className="container">
       <h1>Welcome {name}!</h1>
       <p className="affirmation"></p>
-      <button className="btn">Generate New Affirmation</button>
+      <button className="btn" onClick={fetchRandomAffirmation}>
+        Generate New Affirmation
+      </button>
+
       <select
         className="dropdown"
         value={selectedCategory}
-        onChange={(e) => setSelectedCategory(e.target.value)}
+        onChange={handleCategoryChange}
       >
         <option value="">Select Category</option>
         {category.map((cat, index) => (
-          <option key={index} value={cat.category}>{cat.category}</option>
+          <option key={index} value={cat.category}>
+            {cat.category}
+          </option>
         ))}
       </select>
-      <button className="btn" onClick={() => window.location.href='/create-affirmation'}>Create Affirmation</button>
+      <button
+        className="btn"
+        onClick={() => (window.location.href = "/create-affirmation")}
+      >
+        Create Affirmation
+      </button>
     </div>
   );
 };
